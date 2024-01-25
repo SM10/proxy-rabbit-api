c024d3306a5eac80f768624f4e3492b80474d2c9
Stephen Man <stephenman88@gmail.com>
2024-01-24 21:52:14 -0500

    added route to get users by country id

--- controller/country-controller.js
+++ controller/country-controller.js
@@ -12,4 +12,10 @@ const getCountriesProducts = async (request, response) => {
     response.status(200).send(products);
 }
 
-module.exports = {getCountries, getCountriesProducts}
+const getCountriesUsers = async (request,response)=>{
+    const users = await knex("user").join("country", "user.country_id", "=", "country.id")
+        .select("user.id as id", "user.first_name as first_name", "user.last_name as last_name", "country.id as country_id", "country.name as country_name")
+        .where("country_id", "=", request.params.countryId);
+}
+
+module.exports = {getCountries, getCountriesProducts, getCountriesUsers}
